d1075b8f07a0ba92a6f98c3d25ef9779e65c91d8
nik frank <nikbfrank@gmail.com>
2018-09-23 15:36:19 +0300

    002 navDrop

--- .storybook/index.js
+++ .storybook/index.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 
 import ResponsiveNav from '../lib/ResponsiveNav';
+import NavDrop from '../lib/NavDrop';
+
+import './storybook.css';
 
 const Button = ()=> <button/>;
 
@@ -15,10 +18,54 @@ storiesOf('Button', module)
   ));
 
 
+class NavDropEj extends Component {
+  state = { open: false }
+  toggleMenu = ()=> this.setState(s=> ({ open: !s.open }) )
+  
+  render(){
+    return (
+      <div className='nav-container'>
+        <NavDrop navItems={['Home', 'Contact']}
+                 toggleMenu={this.toggleMenu}
+                 menuOpen={this.state.open}
+                 onClick={(i=> console.log(i))}/>
+      </div>
+    );
+  }
+}
+
+class NavDropEj2 extends Component {
+  state = { open: false }
+  toggleMenu = ()=> this.setState(s=> ({ open: !s.open }) )
+  
+  render(){
+    return (
+      <div className='nav-container ej2'>
+        <NavDrop navItems={['Home', 'Contact']}
+                 toggleMenu={this.toggleMenu}
+                 menuOpen={this.state.open}
+                 onClick={(i=> console.log(i))}>
+          <button onClick={this.toggleMenu}>Menu</button>
+        </NavDrop>
+      </div>
+    );
+  }
+}
+
 storiesOf('ResponsiveNav', module)
   .add('r nav', ()=> (
-    <ResponsiveNav navItems={['Home', 'Contact']}
-                   toggleMenu={(()=>0)}
-                   menuOpen={true}
-                   onClick={(()=>0)} />
+    <div className='ResponsiveNav-rnav'>
+      <ResponsiveNav navItems={['Home', 'Contact']}
+                     toggleMenu={(()=>0)}
+                     menuOpen={true}
+                     onClick={(()=>0)} />
+    </div>
+  ) )
+
+  .add('nav drop', ()=> (
+    <NavDropEj />
+  ) )
+  .add('nav drop2', ()=> (
+    <NavDropEj2 />
   ) );
+
--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const ResponsiveNav = require('./dist/ResponsiveNav.js');
+const NavDrop = require('./dist/NavDrop.js');
+const MenuIcon = require('./dist/MenuIcon.svg.js');
 
 module.exports = {
   ResponsiveNav,
+  NavDrop,
+  MenuIcon,
 };
