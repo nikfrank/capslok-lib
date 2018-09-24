9cd55a2ee6a830dd9d0f1fa189adeff0a681d230
nik frank <nikbfrank@gmail.com>
2018-09-24 21:33:23 +0300

    004, gitignore proper index

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const ResponsiveNav = require('./dist/ResponsiveNav.js');
-const NavDrop = require('./dist/NavDrop.js');
-const MenuIcon = require('./dist/MenuIcon.svg.js');
+const ResponsiveNav = require('./dist/ResponsiveNav.js').default;
+const NavDrop = require('./dist/NavDrop.js').default;
+const MenuIcon = require('./dist/MenuIcon.svg.js').default;
 
 module.exports = {
   ResponsiveNav,
