328db9a474779fb896dd5d657f3e8896f60e16a0
nik frank <nikbfrank@gmail.com>
2018-09-26 13:34:27 +0300

    007 svg name fix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const ResponsiveNav = require('./dist/ResponsiveNav.js').default;
 const NavDrop = require('./dist/NavDrop.js').default;
-const MenuIcon = require('./dist/MenuIcon.svg.js').default;
+const MenuIcon = require('./dist/MenuIcon.js').default;
 
 module.exports = {
   ResponsiveNav,
